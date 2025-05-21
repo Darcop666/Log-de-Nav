746e19b603ecce702fdcf3d0d44cd8365f51a1a3
Darcop666 <minivancedu138@gmail.com>
2025-05-21 08:38:02 +0200

    first commit

---
+++ page_avion/index.js
@@ -0,0 +1,15 @@
+function ajouterAvion () {
+    this.location.href = "../page_avion/ajouterAvion.html"
+}
+
+function modifierAvion () {
+    this.location.href = "../page_avion/modifierAvion.html"
+}
+
+function consulterAvion () {
+    this.location.href = "../page_avion/consulterAvion.html"
+}
+
+function supprimerAvion () {
+    this.location.href = "../page_avion/supprimerAvion.html"
+}
---
+++ page_principale/renvoi.js
@@ -0,0 +1,7 @@
+function page_avion() {
+    this.location.href = "../page_avion/page_avion.html";
+}
+
+function page_route() {
+    this.location.href = "../page_route/page_route.html";
+}
